36fcc1ef1001e6911134e6b262d0db88c34a4582
Jeremy Robin <jeremy.robin@gmail.com>
2014-09-12 12:13:53 -0600

    0.1.1 release

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='featurevectormatrix',
-    version='0.1.0',
+    version='0.1.1',
     description='Python class to encapsulate different representations of large datasets',
     long_description=readme + '\n\n' + history,
     author='Jeremy Robin',
